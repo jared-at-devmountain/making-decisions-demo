08e0562ceda53be247fb91bb5c7318a02ea91b9f
Jared Collier <jared@Jareds-MBP.localdomain>
2022-09-28 15:42:23 -0600

    final battle plays out: pending assignments

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -1,5 +1,5 @@
 let jonSnowAttack = 25
-let jamieLannisterAttack = 35
+let jamieLannisterAttack = 55
 
 if (jonSnowAttack > jamieLannisterAttack) {
     console.log("Jon Snow has better attack than Jamie Lannister")
@@ -29,9 +29,40 @@ if (jonSnowHealth <= jamieLannisterAttack) {
 jonSnowDefense += 25
 
 //Jamie attacks again!
-if (jonSnowHealth <= jamieLannisterAttack - jonSnowDefense) {
+if (jonSnowHealth <= (jamieLannisterAttack - jonSnowDefense)) {
     console.log("Jon Snow has been slain")
 } else {
-    jonSnowHealth -= (jamieLannisterAttack - jonSnowDefense)
+    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
     console.log(`Jon Snow's Health is down to ${jonSnowHealth}`)
 }
+
+//villager throws Jon a health kit, but health can't exceed 100
+if (jonSnowHealth + 50 >= 100) {
+    jonSnowHealth = 100
+} else {
+    jonSnowHealth += 50
+}
+
+console.log('Jon Snow health after the health kit is ' + jonSnowHealth)
+
+let coinLandsHeads = true
+
+if (coinLandsHeads) {
+    console.log("The fight continues")
+} else {
+    console.log("Jon is allowed to run away")
+}
+
+//Jamie attacks Jon 5 times
+for (let i = 0; i < 5; i++) {
+    jonSnowHealth -= jamieLannisterAttack - jonSnowDefense
+    console.log('Jon\'s health is now ' + jonSnowHealth)
+}
+
+//Post-lab assignment 1:
+//Write logic in the for loop that detects if Jon's health gets 0 or less, in which case, you will print that Jon is dead
+//   and you will stop futher loop iterations (see the "break" JS keyword for leaving a loop early)
+
+//Post-lab assignment 2:
+//Write the for loop (including the logic your wrote for the first assignement) as a while loop.
+//   it should behave exactly the same
